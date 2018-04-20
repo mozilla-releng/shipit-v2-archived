876c747ddee026ca90ef73c928505a9406ede179
Rail Aliiev <rail@mozilla.com>
2018-04-20 13:20:00 -0400

    Abort releases (#2)

--- src/views/ListReleases/index.js
+++ src/views/ListReleases/index.js
@@ -71,21 +71,123 @@ export default class ListReleases extends React.Component {
   }
 }
 
-const Release = ({
-  release,
-}) => (
-  <div className="row">
-    <div className="col">
-      <a href={`${TREEHERDER_URL}/#/jobs?repo=${release.project}&revision=${release.revision}`}>
-        <h3>{release.product} <small>{release.version} build{release.build_number}</small>
-        </h3>
-      </a>
-    </div>
-    <div className="col">
-      <TaskProgress phases={release.phases} releaseName={release.name} />
-    </div>
-  </div>
-);
+class Release extends React.Component {
+  static contextTypes = {
+    authController: object.isRequired,
+  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      showModal: false,
+      submitted: props.submitted,
+      errorMsg: null,
+    };
+  }
+
+  open = () => {
+    this.setState({ showModal: true });
+  };
+
+  close = () => {
+    this.setState({
+      showModal: false,
+      errorMsg: null,
+    });
+  };
+
+  abortRelease = async (release) => {
+    // TODO: refactor functions using the API
+    const url = `${API_URL}/releases/${release.name}`;
+    if (!this.context.authController.userSession) {
+      this.setState({ errorMsg: 'Login required!' });
+      return;
+    }
+    const { accessToken } = this.context.authController.getUserSession();
+    const headers = { Authorization: `Bearer ${accessToken}` };
+    try {
+      const response = await fetch(url, { method: 'DELETE', headers });
+      if (!response.ok) {
+        this.setState({ errorMsg: 'Auth failure!' });
+        return;
+      }
+      this.setState({ submitted: true });
+      window.location.reload();
+    } catch (e) {
+      this.setState({ errorMsg: 'Server issues!' });
+      throw e;
+    }
+  };
+
+  renderBody = () => {
+    const { submitted, errorMsg } = this.state;
+    if (errorMsg) {
+      return (
+        <div>
+          <p>{errorMsg}</p>
+        </div>
+      );
+    }
+    if (!submitted) {
+      return (
+        <div>
+          <h4>Are you sure?</h4>
+          <p>
+            The release will be aborted!
+          </p>
+          <small>
+            TC cancel is not implemented. The release will be just marked as abandoned in the API.
+          </small>
+        </div>
+      );
+    }
+    return (
+      <div>Done.</div>
+    );
+  };
+
+  render() {
+    const { release } = this.props;
+    return (
+      <div className="row">
+        <div className="col">
+          <h3>
+            <a href={`${TREEHERDER_URL}/#/jobs?repo=${release.project}&revision=${release.revision}`}>
+              {release.product} <small>{release.version} build{release.build_number}</small>
+            </a>
+            <Button
+              onClick={this.open}
+              bsStyle="danger"
+              disabled={!this.context.authController.userSession}
+            >
+              Abort release
+            </Button>
+          </h3>
+        </div>
+        <Modal show={this.state.showModal} onHide={this.close}>
+          <Modal.Header closeButton>
+            <Modal.Title>Abort a release</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            {this.renderBody()}
+          </Modal.Body>
+          <Modal.Footer>
+            <Button
+              onClick={() => this.abortRelease(release)}
+              bsStyle="danger"
+              disabled={!this.context.authController.userSession}
+            >
+              Kill eet!
+            </Button>
+            <Button onClick={this.close} bsStyle="primary">Close</Button>
+          </Modal.Footer>
+        </Modal>
+        <div className="col">
+          <TaskProgress phases={release.phases} releaseName={release.name} />
+        </div>
+      </div>
+    );
+  }
+}
 
 const TaskProgress = (props) => {
   const { phases, releaseName } = props;
