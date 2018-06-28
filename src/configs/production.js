dc9027f2acea14b83f4d49e3da8c9c75575407df
Johan Lorenzo <jlorenzo@mozilla.com>
2018-06-28 15:52:21 +0200

    s/Firefox Moblie/Firefox Mobile

--- src/configs/production.js
+++ src/configs/production.js
@@ -37,7 +37,7 @@ module.exports = {
     },
     {
       product: 'fennec',
-      prettyName: 'Firefox Moblie',
+      prettyName: 'Firefox Mobile',
       // TODO: The actual appName is `mobile/android` but it gets the version from `browser`.
       appName: 'browser',
       branches: [
